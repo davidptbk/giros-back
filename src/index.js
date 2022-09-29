dc6823ba79f0a9391ded089a97b5bec843fa73c6
David Pineda Tobon <davidpt94@gmail.com>
2022-09-29 09:56:06 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router(`${__dirname}/data/db.json`);
+
+
+const port = process.env.PORT || 3000;
+
+server.use(router)
+server.listen(port, () => {
+    console.log('JSON Server is running')
+})
